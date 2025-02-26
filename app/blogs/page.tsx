ccf545f162249f42c0834b486f7d65c4107717d9
Ahsan Khan <ahsan.ak943@gmail.com>
2025-02-26 21:52:55 +0500

    test: iamge rendering reivew in server component & reloading effect with suspence/loading

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,29 +1,47 @@
 
 import React, { Suspense } from 'react'
-import LoadingBlogs from './loading';
+// import LoadingBlogs from './_loading';
 import Link from 'next/link';
+import Image from 'next/image';
+import LoadingBlogs from './loading';
 
 async function getData() {
-    const data = await fetch("https://api.vercel.app/blog", {
+    const blogsUrl = `${process.env.NEXT_PUBLIC_API_URL!}/product?size=50`;
+
+    const data = await fetch(blogsUrl, {
         cache: "no-store"
         // next: { revalidate: 0 }
     });
 
     if (!data.ok) throw new Error("Failed to fetch data");
-
-    return data.json();
+    const blogs = await data.json()
+    return blogs.data.Adverts
 }
 
 
 async function BlogList() {
-    const todos = await getData();
+    const blogs = await getData();
 
     return (
-        <div>
-            {todos.map((item: { id: string; title: string }) => (
-                <Link className='block' key={item.id} href={`/blogs/${item.id}`}>
-                    {item.title}
-                </Link>
+        <div className='flex space-x-5'>
+            {blogs.map((item: { id: string; title: string, thumbnail: string }) => (
+                <div className='flex flex-col-reverse space-x-3' key={item.id}>
+                    <Image
+                        src={item.thumbnail}
+                        alt={`${item.thumbnail}`}
+                        priority
+                        height={250}
+                        width={250}
+                        sizes="(max-width: 768px) 768, 100vw"
+                        // fetchPriority="high"
+                        className="  h-[250px] w-[250px]  "
+                        id="mainImage"
+                        loading="eager"
+                    />
+                    <Link className='block' href={`/blogs/${item.id}`}>
+                        {item.title}
+                    </Link>
+                </div>
             ))}
         </div>
     );
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kaufes-dev-new.s3.eu-central-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "staging.kaufes.ch",
+      },
+      {
+        protocol: "https",
+        hostname: "kaufes-dev-v2.s3.me-south-1.amazonaws.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
